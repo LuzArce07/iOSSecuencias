02984da79b42554ae210361d7f8b961eda55ade1
Alumno <ulsa@iMacD106.local>
2019-08-20 14:33:47 -0700

    Conejo inicia y para la animacion

--- Secuencias/ViewController.swift
+++ Secuencias/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    
+    @IBAction func DoTapConejo(_ sender: Any) {
+        
+        if imgSecuencia.isAnimating{
+            
+            imgSecuencia.stopAnimating()
+            
+        } else {
+            
+            //Aqui la empezara ya que se le de tap(clic)
+            imgSecuencia.startAnimating()
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Arreglo de UIImage con las imagenes de la secuencia
+        var imagenesSecuenciaConejo : [UIImage] = []
+        
+        //El tipo de dato es opcional se pone el signo de admiracion
+        //Se ponen el nombre de las imagenes
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo1")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo2")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo3")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo4")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo5")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo6")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "conejo7")!)
+        
+        imgSecuencia.animationImages = imagenesSecuenciaConejo
+        imgSecuencia.animationDuration = 0.5 //La duracion es en segundos
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
 }
-
